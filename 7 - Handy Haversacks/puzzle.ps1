356686d70ba7ad9ea54c65405eca96aa8ba0828f
jrankindev <jxrankin@gmail.com>
2021-01-14 11:46:00 -0500

    still not done; this is hard!

--- 7 - Handy Haversacks/puzzle.ps1
+++ 7 - Handy Haversacks/puzzle.ps1
@@ -4,7 +4,6 @@ Clear-Host
 #grab input and put into arraylist
 [System.Collections.ArrayList] $arrayFromFile = Get-Content pie.txt
 
-
 <# sanitize input
 get rid of shiny gold bag entry (not bags that can contain shiny gold bags)
 get rid of bags that can contain no other bags
@@ -38,8 +37,48 @@ Write-Output "Filtered: $($arrayFromFile.Count - $sanitizedArrayFromFile.Count) 
 #search array to store entries we care about, preload with shiny gold
 $searchArray = @("shiny gold")
 
+$newBagFound = $true
+
+Write-Output "Matching:"
+$searchArray
+echo ""
 foreach ($element in $sanitizedArrayFromFile) {
     if ($element -match $searchArray) {
-        Write-Output "MATCH -> $element"
+        Write-Output "TRUE  -> $element"
+    } else {
+        Write-Output "FALSE -> $element"
     }
 }
+
+echo ""
+
+for ($i = 0; $i -lt $sanitizedArrayFromFile.Count; $i++) {
+    [string[]] $elementArray = $sanitizedArrayFromFile[$i].Split(":")
+    $elementArray
+
+    if ($sanitizedArrayFromFile[$i] -match $searchArray[0]) {
+        Write-Output "TRUE  -> $($sanitizedArrayFromFile[$i])"
+    } else {
+        Write-Output "FALSE -> $($sanitizedArrayFromFile[$i])"
+    }
+    echo ""
+}
+
+echo ""
+echo ""
+($sanitizedArrayFromFile | %{$searchArray -like $_}) -contains $true
+
+<#foreach ($element in $sanitizedArrayFromFile) {
+    [string[]] $elementArray = $element.Split(":")
+    for ($i = 1; $i -lt $elementArray.Count; $i++) {
+        if ($elementArray[$i] -match $searchArray) {
+            $elementArray[$i].Remove(0,2)
+            $elementArray[0]
+            $searchArray += $elementArray[0]
+        }
+        #$elementArray[$i].Remove(0,2)
+    }
+    echo ""
+}
+
+$searchArray#>
