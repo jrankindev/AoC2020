db32136ffc0f7f9e7924a17f591789cf671fd5e7
jrankindev <jxrankin@gmail.com>
2021-01-17 15:44:07 -0500

    working on part 2

---
+++ 7 - Handy Haversacks/puzzle_p2.ps1
@@ -0,0 +1,15 @@
+﻿#clear screen
+Clear-Host
+
+#start part 2 text
+Write-Host " Part 2 "  -BackgroundColor "Green" -ForegroundColor "Black"
+
+#grab input and put into arraylist
+[System.Collections.ArrayList] $arrayFromFile = Get-Content pie.txt
+
+foreach ($element in $arrayFromFile) {
+    $element
+}
+
+#show answer
+Write-Host " How many individual bags are required inside your single shiny gold bag? (P2 Answer): XXXXX "  -BackgroundColor "Green" -ForegroundColor "Black"
